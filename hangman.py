443f89dc88d6ba9cd92e74c179403c539d03ba75
shun <mike53669@gmail.com>
2021-02-12 23:45:21 -0800

    hangman v2

--- hangman.py
+++ hangman.py
@@ -1,5 +1,9 @@
 import random
 from words import words
+import string
+
+# lives in hangman
+mistakes = 10
 
 # no abc-def 
 # return a valid word
@@ -9,39 +13,45 @@ def GetValidWord():
         if '-' not in random_select and ' ' not in random_select:
             return random_select
 
+# option2, keeping only 2 sets
+# 1, the alphabet
+# 2, used letters (guessed, right/wrong)
+# 1-2 = new letters
+
+# every single time guess correctly, remove from word_letter
+# word_letter = what's left to be matched
 def Play():
-    mistakes = 5
-    mistake_list = []
     computer_word = GetValidWord()
-    #map of letters => position
-    total_letters = set()
-    for l in computer_word:
-        total_letters.add(l)
+    mistake_set = set() # words that user guessed, but wrong
+    # map of letters => position
+    # set constructor takes in an iterable (string)
+    total_letters = set(computer_word)
     length = len(computer_word)
     print(f"please guess the word: {(' ').join(['_']*length)}") # 3 underscores, middle underscore can be used to replace the letter
+    # return only lower case letters
+    alphabet = set(string.ascii_lowercase)
     partial_result = set() # letters that user has guessed correctly
-    while len(mistake_list) < mistakes:
+    while len(mistake_set) < mistakes:
         user_letter = input("letter: ")
-        if user_letter in mistake_list or user_letter in partial_result:
+        if user_letter not in alphabet:
+            print("invalid input")
+        elif user_letter in mistake_set or user_letter in partial_result:
             print("already guessed")
-        elif user_letter in total_letters:
+        elif user_letter not in total_letters:
+            # guessed wrong
+            mistake_set.add(user_letter)
+            # using set, printing order is not garanteed
+            print(f"you've guess wrong {len(mistake_set)} times: {','.join(mistake_set)}, there are a total of {mistakes} tries")
+        else:
             # guessed correctly
             partial_result.add(user_letter)
             # print word
-            print_word = []
-            for l in computer_word:
-                if l in partial_result:
-                    print_word.append(l)
-                else:
-                    print_word.append("_")
+            print_word = [ l if l in partial_result else '_' for l in computer_word]
             print(f"new result: {(' '.join(print_word))}")
             if len(partial_result) == len(total_letters):
                 print("congrats")
                 return
-        else:
-            mistake_list.append(user_letter)
-            print(f"you've guess wrong {len(mistake_list)} times: {','.join(mistake_list)}, there are total of {mistakes} chances")
-    print(f"correct word is {computer_word}")
+    print(f"correct word is '{computer_word}'")
 
 Play()
 
