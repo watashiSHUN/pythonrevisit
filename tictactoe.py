293a787834c545132fd3ee251406843cf408eab2
shun <mike53669@gmail.com>
2021-02-14 11:25:22 -0800

    optimal tictactoe computer

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
 import random
+from functools import reduce
 
 class Player:
-    # O, X, #, any two letters, the game is played between 2
+    # assumed to be O/X, the game is played between 2
     def __init__(self, symbol):
         self.symbol = symbol
     # takes a game state, play the next move
@@ -27,6 +28,96 @@ class ComputerPlayer(Player):
         next_move = random.choice(list(game.spots()))
         # computer always play a valid move
         game.play(self.symbol, next_move)
+
+# TODO, never takes middle?
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        self.map = {} # state: move, winner at the end if everyone plays optimally
+
+    # total steps 0-8
+    def play(self, game):
+        flat = game.getFlatBoard()
+        move, score = self.findBestMove(flat, self.symbol)
+        # cast move to i,j
+        i = move // 3
+        j = move % 3
+        game.play(self.symbol, (i,j))
+
+    # assume mover will try its best
+    # mover is the next to move
+    def findBestMove(self, game_state, mover_symbol):
+        # base case
+        if game_state in self.map:
+            return self.map[game_state]
+        # compute winner (game ends at this stage)
+        winner = SmartComputerPlayer.computeWinner(game_state)
+        result = None
+        if winner is not None:
+            result = (None, winner)
+        else: # winner == None
+            empty_spots = [i for i,v in enumerate(game_state) if v == ' ']
+            if not empty_spots: # draw
+                result = (None, None)
+            # TODO use game object
+            # try all the moves
+            else:
+                next_symbol = 'X' if mover_symbol=='O' else 'O'
+                base = list(game_state[:])
+                for move in empty_spots:
+                    base[move] = mover_symbol
+                    next_move, winner = self.findBestMove(tuple(base),next_symbol)
+                    base[move] = ' ' # reset for dfs
+                    # cases where we need to update the optimal
+                    if result is None:
+                        result = (move, winner)
+                    # win >= draw >= lose
+                    elif winner == mover_symbol:
+                        result = (move, winner)
+                    elif winner is None and result[1] == next_symbol:
+                        # update
+                        result = (move, winner)
+                    
+        self.map[game_state] = result
+
+        # print('best move:', result, mover_symbol)
+        return result
+                
+
+    # TODO, I don't want to call self
+    # do I need @staticmethod
+    @staticmethod
+    def helper(a,b):
+        return a if a == b and a != ' ' else None
+
+    @staticmethod
+    def printflat(game):
+        for i in range(3):
+            print(",".join(game[i*3:(i+1)*3]))
+
+    @staticmethod
+    def computeWinner(game_state):
+        # row
+        # [[0,1,2].[3,4,5],[6,7,8]]
+        for row in [game_state[i*3:(i+1)*3] for i in range(3)]:
+            result = reduce(SmartComputerPlayer.helper, row)
+            if result:
+                return result
+        # col
+        for col in [[game_state[i*3+j] for i in range(3)] for j in range(3)]:
+            result = reduce(SmartComputerPlayer.helper, col)
+            if result:
+                return result
+        # diagonal
+        diagonal = [0,4,8]
+        inverse_diagona = [2,4,6]
+        result = reduce(SmartComputerPlayer.helper, [game_state[i] for i in diagonal])
+        if result:
+            return result
+        result = reduce(SmartComputerPlayer.helper, [game_state[i] for i in inverse_diagona])
+        if result:
+            return result
+        
 
 class HumanPlayer(Player):
     def play(self, game):
@@ -57,6 +148,8 @@ class Game:
         self.col =[{},{},{}]
         self.diagonal = [{},{}]
 
+    def getFlatBoard(self):
+        return tuple(self.board[i][j] for i in range(3) for j in range(3))
     # first function to define
     def printBoard(self):
         for i in self.board:
@@ -79,6 +172,7 @@ class Game:
     # used to determine draw, or gameover
     def spots(self):
         return self.empty
+
     # return bool, valid play
     def play(self, symbol, coordinate):
         if coordinate in self.empty:
@@ -100,11 +194,11 @@ class Game:
 def driver():
     # create two players
     # players = {'X': ComputerPlayer('X'), 'O': ComputerPlayer('O')}
-    players = {'X': ComputerPlayer('X'), 'O': HumanPlayer('O')}
+    players = {'X': SmartComputerPlayer('X'), 'O': HumanPlayer('O')}
     # alternatively pass the game around players
     # at the end of each term, determine who have won
     game = Game()
-    current_player = players['X']
+    current_player = players['O']
     while game.spots(): # total of 9, 3*3 matrix
         current_player.play(game)
         game.printBoard()
