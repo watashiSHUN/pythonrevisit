df69d1a3aca9fb1c03dc6d8301a47ff4f3277dc5
shun <mike53669@gmail.com>
2021-02-26 23:30:00 -0800

    working v1, expand is incomplete

--- minesweeper.py
+++ minesweeper.py
@@ -11,7 +11,7 @@ class Tile:
     # TODO alternatively, we can use isinstance() and have multiple subclasses
     def __init__(self, tile_type):
         # tile could be undiscovered (this is display state)
-        self.discovered = True # default should be false
+        self.discovered = False # default should be false
         self.type = tile_type
         self.num = 0 # TODO wasted for the other 2 types
 
@@ -45,12 +45,10 @@ class Tile:
         self.num += number
         return self
 
-    def isBomb(self):
-        return self.type is TileType.BOMB
-
-# use bfs to expand a minesweeper
+# use bfs? as an exercises, to expand a minesweeper
 class Game:
     def __init__(self, size):
+        self.undiscovered = size*size
         self.dimension = size
         # size*size
         self.matrix = []
@@ -60,7 +58,7 @@ class Game:
             for j in range(size):
                 row.append(Tile(TileType.SPACE))
             self.matrix.append(row)
-        # flatten thematrix, we can use index to specify which ones are mine
+        # flatten the matrix, we can use index to specify which ones are mine
         # random.choices return result with sampling
         bombs = random.sample([i for i in range(size*size)], size)
 
@@ -82,14 +80,85 @@ class Game:
     def convert(self, integer):
         # return (i,j)
         return(integer//self.dimension, integer%self.dimension)
-        
+
     def print(self):
         for row in self.matrix:
             print("|"," | ".join([str(c) for c in row]),"|")
+    
+    def reveal(self):
+        # make all tiles discovered
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                self.matrix[i][j].discovered = True
+
+    def expand(self,i,j):
+        # base case
+        # hit a boundary
+        if i < 0 or i >= self.dimension or j < 0 or j >= self.dimension:
+            return
+        if self.matrix[i][j].discovered: # already processed
+            return
+        tile_type = self.matrix[i][j].type
+        self.revealPosition(i,j)
+        # cannot reach any where from a number
+        if tile_type is TileType.NUMBER:
+            # end
+            return
+        # recurse, up/down/left/right
+        # must be space
+        assert(tile_type is TileType.SPACE)
+        for x,y in [(1,0),(-1,0),(0,1),(0,-1)]:
+            self.expand(i+x,j+y)
+
+    def revealPosition(self, i,j):
+        self.matrix[i][j].discovered = True
+        self.undiscovered -= 1
+    
+    def isUserWin(self):
+        self.undiscovered == self.dimension
+
+    def Play(self, i,j): # discover a tile
+        # sanity check
+        if 0 > i or i >= self.dimension:
+            print(f"out of bound, 0 <= i < {self.dimension}")
+            return False
+        # 1 if out of bound, return false
+        if 0 > j or j >= self.dimension:
+            print(f"out of bound, 0 <= j < {self.dimension}")
+            return False
+        # 2 if already discovered, return false
+        if self.matrix[i][j].discovered:
+            print(f"already played {(i,j)}")
+            return False
+        # 3 if bomb return True (gg)
+        tile_type = self.matrix[i][j].type
+        if tile_type is TileType.BOMB:
+            self.revealPosition(i,j)
+            print("ooops, you lost")
+            return True
+        # anything below can be the winning condition
+        # 4 if we hit a number
+        if tile_type is TileType.NUMBER:
+            self.revealPosition(i,j)
+        # 5 if we hit a space, we will discover all tiles until the boundary
+        # probably dfs, if we use bfs the intermediate queue will get huge
+        elif tile_type is TileType.SPACE:
+            self.expand(i,j)
+        if self.isUserWin():
+            print("you won")
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
-    g = Game(10)
-    g.print()
+    g = Game(5)
+    while True:
+        g.print()
+        i,j = [int(x) for x in input("pick a position to dig (i,j):").split(',')]
+        if g.Play(i,j):
+            g.reveal()
+            g.print()
+            break
 
 
 
