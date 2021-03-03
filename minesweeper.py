a7d4a15efef47f0851f24ecb019d450cf88d516b
shun <mike53669@gmail.com>
2021-03-02 23:27:06 -0800

    minesweeper v3

--- minesweeper.py
+++ minesweeper.py
@@ -4,7 +4,8 @@
 import random
 from enum import Enum
 
-TileType = Enum("TileType", "SPACE NUMBER BOMB")
+TileType = Enum("TileType", "NUMBER BOMB")
+GameState = Enum("GameState", "WIN LOSE CONTINUE")
 
 
 class Tile:
@@ -14,7 +15,7 @@ class Tile:
         # tile could be undiscovered (this is display state)
         self.discovered = False  # default should be false
         self.type = tile_type
-        self.num = 0  # TODO wasted for the other 2 types
+        self.num = 0
         # by definition, 0/ space just means that there's zero bombs around it
         # very consistent definition
 
@@ -24,9 +25,7 @@ class Tile:
     # enum vs class?
     def __str__(self):
         if self.discovered:
-            if self.type is TileType.SPACE:
-                return "_"
-            elif self.type is TileType.NUMBER:
+            if self.type is TileType.NUMBER:
                 return str(self.num)
             else:
                 return "*"
@@ -70,7 +69,7 @@ class Game:
             # add a row
             row = []
             for j in range(self.dimension):
-                row.append(Tile(TileType.SPACE))
+                row.append(Tile(TileType.NUMBER))
             self.matrix.append(row)
         # flatten the matrix, we can use index to specify which ones are mine
         # random.choices return result with sampling
@@ -86,6 +85,7 @@ class Game:
     def updateSurroundings(self, i, j):
         # caveat: right end +2
         # caveat: dont confuse the min max
+        # max(0,x) => at least 0
         for x in range(max(0, i - 1), min(i + 2, self.dimension)):
             for y in range(max(0, j - 1), min(j + 2, self.dimension)):
                 if x == i and y == j:
@@ -96,36 +96,21 @@ class Game:
         # return (i,j)
         return (integer // self.dimension, integer % self.dimension)
 
-    def print(self):
+    # return a string
+    def __str__(self):
+        visible_board = []
         for row in self.matrix:
-            print("|", " | ".join([str(c) for c in row]), "|")
+            visible_board.append("| ")
+            visible_board.append(" | ".join([str(c) for c in row]))
+            visible_board.append(" |")
+            visible_board.append("\n")
+        return "".join(visible_board)
 
     def reveal(self):
         # make all tiles discovered
         for i in range(self.dimension):
             for j in range(self.dimension):
                 self.matrix[i][j].discovered = True
-
-    def expand(self, i, j):
-        # base case
-        # hit a boundary
-        if i < 0 or i >= self.dimension or j < 0 or j >= self.dimension:
-            return
-        if self.matrix[i][j].discovered:  # already processed
-            return
-        tile_type = self.matrix[i][j].type
-        self.revealPosition(i, j)
-        # cannot reach any where from a number
-        # WHEN YOU REACHED A NUMBER, THEN IT IS NEXT TO A BOMB
-        if tile_type is TileType.NUMBER:
-            # end
-            return
-        # recurse, up/down/left/right
-        # must be space
-        assert tile_type is TileType.SPACE
-        const = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for x, y in const:
-            self.expand(i + x, j + y)
 
     def revealPosition(self, i, j):
         self.matrix[i][j].discovered = True
@@ -134,50 +119,60 @@ class Game:
     def isUserWin(self):
         return self.undiscovered == self.bombs
 
-    def Play(self, i, j):  # discover a tile
-        # sanity check
-        if 0 > i or i >= self.dimension:
-            print(f"out of bound, 0 <= i < {self.dimension}")
-            return False
-        # 1 if out of bound, return false
-        if 0 > j or j >= self.dimension:
-            print(f"out of bound, 0 <= j < {self.dimension}")
-            return False
-        # 2 if already discovered, return false
+    # don't do boundary check
+    # stop when we hit a bomb/number>1
+    # otherwise recursively explore
+
+    # return 3 possible state
+    def Play(self, i, j):
+        # if already discovered
         if self.matrix[i][j].discovered:
-            print(f"already played {(i,j)}")
-            return False
-        # 3 if bomb return True (gg)
-        tile_type = self.matrix[i][j].type
-        if tile_type is TileType.BOMB:
+            return GameState.CONTINUE
+
+        if self.matrix[i][j].type is TileType.BOMB:
+            return GameState.LOSE
+
+        # either case can return a win
+        if self.matrix[i][j].num > 0:
             self.revealPosition(i, j)
-            print("ooops, you lost")
-            return True
-        # anything below can be the winning condition
-        # 4 if we hit a number
-        if tile_type is TileType.NUMBER:
-            self.revealPosition(i, j)
+
         # 5 if we hit a space, we will discover all tiles until the boundary
         # probably dfs, if we use bfs the intermediate queue will get huge
-        elif tile_type is TileType.SPACE:
-            self.expand(i, j)
+        if self.matrix[i][j].num == 0:
+            self.revealPosition(i, j)
+            for x in range(max(0, i - 1), min(i + 2, self.dimension)):
+                for y in range(max(0, j - 1), min(j + 2, self.dimension)):
+                    if x == i and y == j:
+                        continue
+                    # expand
+                    self.Play(x, y)
+
         if self.isUserWin():
-            print("you won")
-            return True
+            return GameState.WIN
         else:
-            return False
+            return GameState.CONTINUE
 
 
 if __name__ == "__main__":
     # create the game
-    g = Game()
+    dimension = 5
+    bombs = 2
+    g = Game(size=dimension, num_bombs=bombs)
     while True:
-        g.print()
+        print(g)
         try:
             i, j = [int(x) for x in input("pick a position to dig (i,j):").split(",")]
-            if g.Play(i, j):
+            # sanity check
+            if i < 0 or j < 0 or i >= dimension or j >= dimension:
+                raise ValueError
+            game_state = g.Play(i, j)
+            if game_state is not GameState.CONTINUE:
                 g.reveal()
-                g.print()
+                print(g)
+                if game_state is GameState.WIN:
+                    print("you won")
+                else:
+                    print("you lost")
                 break
         except ValueError:  # if we don't do this, it will catch interrupt, cannot exit from interpreter
             # value error
