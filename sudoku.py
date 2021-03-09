7ed74d09e3b907f68c397566a869328ba8f49446
shun <mike53669@gmail.com>
2021-03-08 22:37:39 -0800

    refactor is_valid and findCube

--- sudoku.py
+++ sudoku.py
@@ -5,28 +5,31 @@
 # ]
 
 # cubes 0-2, 3-5, 6-8
-def findCube(index):
-    # return a list of size 3*3, convert to matrix index puzzle[row][col]
-    single = index % 9
-    double = index // 9
-    if 0 <= single <= 2:
-        s = [0, 1, 2]
-    elif 3 <= single <= 5:
-        s = [3, 4, 5]
-    else:
-        s = [6, 7, 8]
-
-    if 0 <= double <= 2:
-        d = [0, 1, 2]
-    elif 3 <= double <= 5:
-        d = [3, 4, 5]
-    else:
-        d = [6, 7, 8]
+def findCube(row, col):
+    # // 3 => 0,1,2=0; 3,4,5=1; 6,7,8=2;
     returnV = []
-    for i in d:
-        for j in s:
-            returnV.append(i * 9 + j)
+    for i in range((row // 3) * 3, (row // 3) * 3 + 3):
+        for j in range((col // 3) * 3, (col // 3) * 3 + 3):
+            returnV.append((i, j))
     return returnV
+
+
+def is_valid(puzzle, guess, row, col):
+    # 3 things to check
+    # row
+    # x is the element
+    if sum([1 for x in puzzle[row] if x == guess]) > 0:
+        return False
+    # col
+    # x is the row index
+    if sum([1 for x in range(9) if puzzle[x][col] == guess]) > 0:
+        return False
+    # cube
+    for cube_row, cube_col in findCube(row, col):
+        if puzzle[cube_row][cube_col] == guess:
+            return False
+
+    return True
 
 
 # index to the missing[]
@@ -39,30 +42,10 @@ def dfs(missing, index, puzzle):
     col = missing[index] % 9
     # in current_position, try 1->9
     for i in range(1, 10):
-        # make sure current number works
-        # 3 things to check
-        # row
-        # x is the element
-        if sum([1 for x in puzzle[row] if x == i]) > 0:
-            continue  # skip
-        # col
-        # x is the row index
-        if sum([1 for x in range(9) if puzzle[x][col] == i]) > 0:
-            continue  # skip
-        # cube
-        flag = False
-        for cube in findCube(missing[index]):
-            cube_row = cube // 9
-            cube_col = cube % 9
-            if puzzle[cube_row][cube_col] == i:
-                flag = True
-                break
-        if flag:
-            continue  # skip
-        # set index to i
-        puzzle[row][col] = i
-        if dfs(missing, index + 1, puzzle):
-            return True
+        if is_valid(puzzle, i, row, col):
+            puzzle[row][col] = i
+            if dfs(missing, index + 1, puzzle):
+                return True
     # if not find, need to set it to -1, otherwise the row/col/cube comparison will fail
     puzzle[row][col] = -1
     # don't need to set it back to -1 since we have missings (all necessary)
@@ -92,3 +75,4 @@ if __name__ == "__main__":
         [1, -1, 9, -1, -1, -1, 2, -1, -1],
     ]
     print(solve_sudoku(puzzle))
+    print(puzzle)
