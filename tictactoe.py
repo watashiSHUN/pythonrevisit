b292ca03f1fb1895dc59801138171cd36e7cb20d
shun <mike53669@gmail.com>
2021-03-10 23:51:56 -0800

    auto format tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,20 @@
 import random
 from functools import reduce
 
+
 class Player:
     # assumed to be O/X, the game is played between 2
     def __init__(self, symbol):
         self.symbol = symbol
+
     # takes a game state, play the next move
     # for inheritance
     def play(self, game):
         pass
+
     # TODO, python doesn't need the template design pattern
     # dynamic type???
+
 
 # TODO how to test correctness
 # inheritance
@@ -20,30 +24,35 @@ class Player:
 # TODO, then why the inheritance
 # since we are calling parent constructor, we get parent member variable self.parent_field...
 class ComputerPlayer(Player):
-    def __init__(self,letter):
+    def __init__(self, letter):
         super().__init__(letter)
 
-    def play(self, game): # override
+    def play(self, game):  # override
         # randomly pick a spot to play
         # you can not index into a set, only array
         next_move = random.choice(list(game.spots()))
         # computer always play a valid move
         game.play(self.symbol, next_move)
 
+
 # TODO, never takes middle?
 class SmartComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
-        self.map = {} # state: move, winner at the end if everyone plays optimally + how many empty spots left (fewer step is superior)
+        self.map = {}
+        # key = state:
+        # value =
+        # <move, winner at the end if everyone plays optimally, how many empty spots left (fewer step is superior)>
+        # TODO(shunxian) override a comparison
 
     # total steps 0-8
     def play(self, game):
         flat = game.getFlatBoard()
-        move, score, empties = self.findBestMove(flat, self.symbol)
+        move = self.findBestMove(flat, self.symbol)[0]
         # cast move to i,j
         i = move // 3
         j = move % 3
-        game.play(self.symbol, (i,j))
+        game.play(self.symbol, (i, j))
 
     # return best move for mover_symbol
     # assume the game_state is not over yet
@@ -53,7 +62,7 @@ class SmartComputerPlayer(Player):
             return self.map[game_state]
         result = None
         # compute possible places to put mover_sybmol
-        empty_list = [i for i,v in enumerate(game_state) if v == ' ']
+        empty_list = [i for i, v in enumerate(game_state) if v == " "]
         empties = len(empty_list)
         next_state = list(game_state[:])
         for e in empty_list:
@@ -62,7 +71,7 @@ class SmartComputerPlayer(Player):
             # compute if this is a winning move
             winner = SmartComputerPlayer.computeWinner(next_state, mover_symbol, e)
             if winner == mover_symbol or empties == 1:
-                self.map[game_state] = (e, winner, empties-1)
+                self.map[game_state] = (e, winner, empties - 1)
                 return self.map[game_state]
             # if we won or draw, the game_state can not progress further
             # the result can be used to return
@@ -70,59 +79,61 @@ class SmartComputerPlayer(Player):
             # make one move we tie => only one move can be made
             else:
                 # the game has not finished, let opponent play
-                opp_symbol = 'X' if mover_symbol == 'O' else 'O'
-                opp_move, opp_winner, opp_empties = self.findBestMove(tuple(next_state), opp_symbol)
+                opp_symbol = "X" if mover_symbol == "O" else "O"
+                opp_move, opp_winner, opp_empties = self.findBestMove(
+                    tuple(next_state), opp_symbol
+                )
                 if result is None:
                     result = (e, opp_winner, opp_empties)
                 # result has value already
                 # old result => I lost
                 elif result[1] == opp_symbol:
                     # tie/win or I still lost, but lost less
-                    if opp_winner != opp_symbol or empties-1 < result[2]:
+                    if opp_winner != opp_symbol or empties - 1 < result[2]:
                         result = (e, opp_winner, opp_empties)
                 # old result => I won
                 elif result[1] == mover_symbol:
                     # I won and with more empties
-                    if opp_winner == mover_symbol and empties-1 > result[2]:
+                    if opp_winner == mover_symbol and empties - 1 > result[2]:
                         result = (e, opp_winner, opp_empties)
                 else:
                     # old result = draw
                     if opp_winner == mover_symbol:
                         result = (e, opp_winner, opp_empties)
-            next_state[e] = ' ' # reset for dfs
+            next_state[e] = " "  # reset for dfs
         self.map[game_state] = result
-        # SmartComputerPlayer.printflat(game_state)
-        # print('best move:', result, mover_symbol)
+        SmartComputerPlayer.printflat(game_state)
+        print("best move:", result, mover_symbol)
         return result
 
     @staticmethod
     def printflat(game):
         for i in range(3):
-            print(",".join(game[i*3:(i+1)*3]))
+            print(",".join(game[i * 3 : (i + 1) * 3]))
 
     # only the last step can produce a winner
     # otherwise the game has already finished => compute winner after each play
     # test only the last step (index)
     # which is why we compute winner after each play
-    # TODO winner(move, letter) -> evaluate if this move makes letter a winner 
+    # TODO winner(move, letter) -> evaluate if this move makes letter a winner
     @staticmethod
     def computeWinner(game_state, mover_symbol, last_move):
-        row = last_move//3
-        if (game_state[row*3:(row+1)*3]).count(mover_symbol) == 3:
+        row = last_move // 3
+        if (game_state[row * 3 : (row + 1) * 3]).count(mover_symbol) == 3:
             return mover_symbol
-        col = last_move%3
-        if [game_state[i*3+col] for i in range(3)].count(mover_symbol) == 3:
+        col = last_move % 3
+        if [game_state[i * 3 + col] for i in range(3)].count(mover_symbol) == 3:
             return mover_symbol
         # diagonal
-        diagonal = [0,4,8]
-        inverse_diagonal = [2,4,6]
+        diagonal = [0, 4, 8]
+        inverse_diagonal = [2, 4, 6]
         if last_move in diagonal:
             if [game_state[i] for i in diagonal].count(mover_symbol) == 3:
                 return mover_symbol
         if last_move in inverse_diagonal:
             if [game_state[i] for i in inverse_diagonal].count(mover_symbol) == 3:
                 return mover_symbol
-        
+
 
 class HumanPlayer(Player):
     def play(self, game):
@@ -131,8 +142,8 @@ class HumanPlayer(Player):
             user_input = input("coordinate(expected: i,j): ")
             # surround user input in try_catch
             try:
-                i,j = user_input.split(',')
-                coord = (int(i),int(j))
+                i, j = user_input.split(",")
+                coord = (int(i), int(j))
                 # could raise invalid error in the gameplay function
                 if game.play(self.symbol, coord):
                     return
@@ -141,6 +152,7 @@ class HumanPlayer(Player):
             except ValueError:
                 print("invalid input, Try again")
 
+
 # player be aware of the game, but the game doesn't need to know about players(passive)
 # store the games
 # easier to play, easier to tally
@@ -148,20 +160,22 @@ class HumanPlayer(Player):
 # empty set => easier for random computer to play
 class Game:
     def __init__(self):
-        self.board = [ [' ']*3 for i in range(3)]
+        self.board = [[" "] * 3 for i in range(3)]
         # used to play (pick a random point)
         # also used to calculate lines (set or list?)
-        self.empty = set([(i,j) for i in range(3) for j in range(3)])
-        self.row =[{},{},{}] # maps => {'x': how many}
-        self.col =[{},{},{}]
-        self.diagonal = [{},{}]
+        self.empty = set([(i, j) for i in range(3) for j in range(3)])
+        self.row = [{}, {}, {}]  # maps => {'x': how many}
+        self.col = [{}, {}, {}]
+        self.diagonal = [{}, {}]
 
     def getFlatBoard(self):
         return tuple(self.board[i][j] for i in range(3) for j in range(3))
+
     # first function to define
     def printBoard(self):
         for i in self.board:
-            print('|', " | ".join(i), '|')
+            print("|", " | ".join(i), "|")
+
     # return winner char
     # inconclusive return None
     def determineWinner(self):
@@ -170,10 +184,11 @@ class Game:
                 # each iter is a list of dictionary
                 for d in iter:
                     if len(d) == 1:
-                    # get the only key,value pair
-                        for k,v in d.items():
+                        # get the only key,value pair
+                        for k, v in d.items():
                             if v == 3:
                                 return k
+
         # values updated in play()
         return helper(self.row, self.col, self.diagonal)
 
@@ -184,30 +199,32 @@ class Game:
     # return bool, valid play
     def play(self, symbol, coordinate):
         if coordinate in self.empty:
-            i,j = coordinate[0],coordinate[1]
+            i, j = coordinate[0], coordinate[1]
             self.board[i][j] = symbol
             self.empty.remove(coordinate)
             # update row/col/diagonal
-            self.row[i][symbol] = self.row[i].get(symbol,0)+1
-            self.col[j][symbol] = self.col[j].get(symbol,0)+1
+            self.row[i][symbol] = self.row[i].get(symbol, 0) + 1
+            self.col[j][symbol] = self.col[j].get(symbol, 0) + 1
             # diagonal[0] = i,i; diagonal[1] = i,2-i
             if i == j:
-                self.diagonal[0][symbol] = self.diagonal[0].get(symbol,0)+1
-            if i == 2-j:
-                self.diagonal[1][symbol] = self.diagonal[1].get(symbol,0)+1
+                self.diagonal[0][symbol] = self.diagonal[0].get(symbol, 0) + 1
+            if i == 2 - j:
+                self.diagonal[1][symbol] = self.diagonal[1].get(symbol, 0) + 1
             return True
         else:
             return False
 
+
 def driver():
     # create two players
-    players = {'X': SmartComputerPlayer('X'), 'O': SmartComputerPlayer('O')}
+    # players = {'X': SmartComputerPlayer('X'), 'O': SmartComputerPlayer('O')}
     # players = {'X': SmartComputerPlayer('X'), 'O': HumanPlayer('O')}
+    players = {"X": SmartComputerPlayer("X"), "O": ComputerPlayer("O")}
     # alternatively pass the game around players
     # at the end of each term, determine who have won
     game = Game()
-    current_player = players['X']
-    while game.spots(): # total of 9, 3*3 matrix
+    current_player = players["X"]
+    while game.spots():  # total of 9, 3*3 matrix
         current_player.play(game)
         game.printBoard()
         # TODO check winner after each move
@@ -216,8 +233,11 @@ def driver():
             print("winner is ", winner)
             return
         # switch players
-        current_player = players['X'] if current_player == players['O'] else players['O']
-        print() #newline
+        current_player = (
+            players["X"] if current_player == players["O"] else players["O"]
+        )
+        print()  # newline
     print("game is a draw")
+
 
 driver()
