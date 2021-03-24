672f266ba69771d1483ece04b7599a3ed1d06d54
shun <mike53669@gmail.com>
2021-03-24 02:29:55 -0700

    snake draft2

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 from enum import Enum
 
 import pygame
@@ -63,22 +64,69 @@ class Direction(Enum):
 class SnakeGame:
     def __init__(self, game_width, game_height):
         # start from top left, going right
-        self.length = 1
         self.direction = Direction.RIGHT # up left down right 
         # matrix width and height, when we draw, each grid is a cube
         self.game_width = game_width
         self.game_height = game_height
-        self.snake = # double ended queue
+        # left is tail, right is head, each entry is a tuple (x,y) coordinate in game grid
+        # popleft(), append()
+        # NOTE: we can obtain the length from this internal data structure, we don't need to store it
+        self.snake = deque([(0,0)])
+        self.apple = ()
 
-    def move_snake(self, direction):
-        # return state 
+    def move_snake(self, direction=None):
+        # compute next step
+        if direction is None or self.same_or_opposite_direction(direction):
+            direction = self.direction
+        # make a turn
+        next_step = self.next_step(direction,*self.snake[-1])
+        # also update the direction
+        self.direction = direction
+        # return state
         # 1. increase length
-        # 2. hit a snag
+        if next_step = self.apple:
+            self.update_snake(next_step, increase_length=True)
+            self.update_apple() # FIXME called outside
+            return 1
+        # 2. hit a wall or your own body
+        if self.out_of_bound(*next_step) or next_step in self.snake:
+            return 2
         # 3. continue moving
+        self.update_snake(next_step, increase_length=False)
+        return 3
 
-        # if no direction is provided or the move can not be completed, we continue
+    def same_or_opposite_direction(self, direction):
+        if (direction is Direction.UP or direction is Direction.DOWN) and (self.direction is Direction.UP or self.direction is Direction.DOWN):
+            return True
+        if (direction is Direction.LEFT or direction is Direction.RIGHT) and (self.direction is Direction.LEFT or self.direction is Direction.RIGHT):
+            return True
+        return False
+
+    def next_step(self, direction, previous_x, previous_y):
+        if direction is Direction.UP:
+            return (previous_x-1, previous_y)
+        elif direction is Direction.DOWN:
+            return (previous_x+1, previous_y)
+        elif direction is Direction.LEFT:
+            return (previous_x, previous_y-1)
+        else:
+            return (previous_x, previous_y+1)
+
+    def out_of_bound(self, x,y):
+        if x < 0 or x >= self.game_width:
+            return True
+        if y < 0 or y >= self.game_height:
+            return True
+        return False
+    
+    def update_snake(self, next_step, increase_length=False):
+        self.snake.append(next_step)
+        if(not increase_length):
+            self.snake.popleft()
+
     def create_apple(self):
         # called if move_snake takes the apple
+
 # after each user move
 # compute possible result
 # then draw
