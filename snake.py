8d9d7c68e9e157a7d99552de08de48ca4ba3ec50
shun <mike53669@gmail.com>
2021-03-25 23:57:26 -0700

    snake draft3 with visuals

--- snake.py
+++ snake.py
@@ -6,55 +6,6 @@ from enum import Enum
 import pygame
 from pygame.math import Vector2
 
-pygame.init()
-
-cell_size = 40
-cell_width = 20
-# create a display surface, main game window
-# NOTE: canvas, there can only be one, display by default
-screen = pygame.display.set_mode(size=(cell_size * cell_width, cell_size * cell_width))
-# it knows that it needs to create a window, but doesn't know how long it should keep it
-# so it creates it and then closes it
-clock = pygame.time.Clock()
-
-# surfaces = layers, we can put stuff on
-# create a surface(import image/write text/create empty) + display surface
-test_surface = pygame.Surface(size=(100, 200))
-# rectangle
-# x, y(NOTE:coordinate of the topleft, NOT the center), width, height
-test_rectangle = pygame.Rect(100, 200, 100, 100)  # less processing power
-# TODO, diff between rect and surface
-# TODO, surface.getrect()
-x_position = 200
-
-# game loop
-while True:
-    # event loop: 1, up down left right 2, close the window, clicking "X"
-    for event in pygame.event.get():
-        # at the beginning of each loop, we try all possible events
-        if event.type == pygame.QUIT:
-            pygame.quit()  # the opposite of pygame.init()
-            sys.exit()
-    # draw all our elements
-
-    # color->RBG tuple (255[100% of red],255,255)
-    # screen.fill(pygame.Color("gold"))
-    # NOTE: fill() is a method unique to screen
-    # rect color is specified in draw (rect object is just a shape)
-    screen.fill((175, 215, 70))
-    # display on the main_display
-    # x,y topleft coordinate
-    # NOTE: this is how we make animation
-    x_position += 1
-    # screen.blit(test_surface, (x_position, 250))
-    # surface, color, object
-    pygame.draw.rect(screen, pygame.Color("gold"), test_rectangle)
-    pygame.display.update()
-    # NOTE: run as many times as possible, depends on the computer
-    # for fast computer, it can run a lot more times than a slow computer
-    # each computer, we move the object for different amount because of this
-    clock.tick(60)  # 60 times in a sec
-
 
 # snake game, creates a grid on display (move 1 grid per frame)
 # how to store snakes?
@@ -72,12 +23,13 @@ class SnakeGame:
     LEFT_VECTOR = Vector2(-1, 0)
     RIGHT_VECTOR = LEFT_VECTOR * -1
 
-    def __init__(self, game_width, game_height):
+    def __init__(self, game_width, game_height, game_cell):
         # start from top left, going right
         self.direction = Direction.RIGHT  # up left down right
         # matrix width and height, when we draw, each grid is a cube
         self.game_width = game_width
         self.game_height = game_height
+        self.game_cell = game_cell
         # left is tail, right is head, each entry is a tuple (x,y) coordinate in game grid
         # popleft(), append()
         # NOTE: we can obtain the length from this internal data structure, we don't need to store it
@@ -156,9 +108,73 @@ class SnakeGame:
                 self.apple = next_apple
                 return
 
-    def draw(self):
+    def draw(self, surface):
         # draw the snake and the apple on to the canvas
-        pass
+        # rectangle
+        # x, y(NOTE:coordinate of the topleft, NOT the center), width, height
+        apple_rect = pygame.Rect(
+            # NOTE: the actual pixel postion = cell_index * cell_width
+            self.apple.x * self.game_cell,
+            self.apple.y * self.game_cell,
+            self.game_cell,
+            self.game_cell,
+        )
+        # surface(screen), color, object
+        pygame.draw.rect(surface, pygame.Color("Red"), apple_rect)
+
+        for snake_body in self.snake:
+            snake_rect = pygame.Rect(
+                snake_body.x * self.game_cell,
+                snake_body.y * self.game_cell,
+                self.game_cell,
+                self.game_cell,
+            )
+            pygame.draw.rect(surface, pygame.Color("Gold"), snake_rect)
+
+
+pygame.init()
+cell_size = 40
+cell_width = 20
+# create a display surface, main game window
+# NOTE: canvas, there can only be one, display by default
+screen = pygame.display.set_mode(size=(cell_size * cell_width, cell_size * cell_width))
+# it knows that it needs to create a window, but doesn't know how long it should keep it
+# so it creates it and then closes it
+clock = pygame.time.Clock()
+
+# surfaces = layers, we can put stuff on
+# create a surface(import image/write text/create empty) + display surface
+test_surface = pygame.Surface(size=(100, 200))
+# TODO, diff between rect and surface
+# TODO, surface.getrect()
+x_position = 200
+
+s = SnakeGame(cell_width, cell_width, cell_size)
+# game loop
+while True:
+    # event loop: 1, up down left right 2, close the window, clicking "X"
+    for event in pygame.event.get():
+        # at the beginning of each loop, we try all possible events
+        if event.type == pygame.QUIT:
+            pygame.quit()  # the opposite of pygame.init()
+            sys.exit()
+    # draw all our elements
+
+    # color->RBG tuple (255[100% of red],255,255)
+    # screen.fill(pygame.Color("gold"))
+    # NOTE: fill() is a method unique to screen
+    # rect color is specified in draw (rect object is just a shape)
+    screen.fill((175, 215, 70))
+    # TODO
+    # 1. take input
+    # 2. move snake accordingly
+    # 3. draw
+    s.draw(screen)
+    pygame.display.update()
+    # NOTE: run as many times as possible, depends on the computer
+    # for fast computer, it can run a lot more times than a slow computer
+    # each computer, we move the object for different amount because of this
+    clock.tick(60)  # 60 times in a sec
 
 
 # after each user move
